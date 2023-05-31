fdd4b8896253196fed5e4c799350e118fa63f2c5
danin <chhindanin@gmail.com>
2023-05-31 12:53:35 +0800

    Initial Commit

---
+++ testing1/testing1App.swift
@@ -0,0 +1,17 @@
+//
+//  testing1App.swift
+//  testing1
+//
+//  Created by ctssadmin on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct testing1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
